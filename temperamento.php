86b6158ba3f87b1f621f177abec1c99e735ace6d
BrunoBernardoHenriques <aalegriadeviver22@gmail.com>
2020-09-29 08:17:00 -0300

    Inicio do desenvolvimento do plugin

---
+++ temperamento.php
@@ -0,0 +1,16 @@
+<?php 
+
+/**
+* Plugin Name: Temperamento
+* Description : Plugin que faz previsão do tempo
+* Version: 1.0
+* Author: Bruno Bernardo Henriques Cardoso
+* Author URI: https://github.com/BrunoBernardoHenriques
+
+*/
+
+function temperamento(){
+	return '<h3>OLÁ </h3>';
+}
+
+add_shortcode('temperamento', 'temperamento');
